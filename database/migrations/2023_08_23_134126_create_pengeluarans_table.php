5ddc53e77286a63dd003f08556eba28e4b6680c9
rahanpas <rahanpas@yahoo.co.id>
2023-08-23 20:59:24 +0700

    pengeluaran

---
+++ database/migrations/2023_08_23_134126_create_pengeluarans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengeluarans', function (Blueprint $table) {
+            $table->id();
+            $table->longText('infopengeluaran');
+            $table->float('jumlah', 17, 2);
+            $table->string('pathfile');
+            $table->string('namafile');
+            $table->dateTime('tanggalbon', 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengeluarans');
+    }
+};
